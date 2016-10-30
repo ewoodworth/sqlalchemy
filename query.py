a589fc7c87da676e45f9a628d03d5a360d94f304
vagrant <vagrant@vagrant.vm>
2016-10-30 19:24:42 +0000

    Using dict to sort out sqlalchemy query

--- query.py
+++ query.py
@@ -18,37 +18,43 @@ init_app()
 # -------------------------------------------------------------------
 # Part 2: Write queries
 
-
 # Get the brand with the **id** of 8.
-
+Brand.query.get(8)
 # Get all models with the **name** Corvette and the **brand_name** Chevrolet.
-
+Model.query.filter(Model.name == 'Corvette', Model.brand_name == 'Chevrolet').all()
 # Get all models that are older than 1960.
-
+Model.query.filter(Model.year < 1960).all()
 # Get all brands that were founded after 1920.
-
+Brand.query.filter(Brand.founded >1920).all()
 # Get all models with names that begin with "Cor".
-
+Model.query.filter(Model.name.like('Cor%')).all()
 # Get all brands that were founded in 1903 and that are not yet discontinued.
-
+Brand.query.filter(Brand.founded == 1903, Brand.discontinued == None).all()
 # Get all brands that are either 1) discontinued (at any time) or 2) founded
 # before 1950.
-
+Brand.query.filter((Brand.discontinued.isnot(None))|(Brand.founded < 1950)).all()
 # Get all models whose brand_name is not Chevrolet.
-
+Model.query.filter(Model.brand_name != 'Chevrolet').all()
 # Fill in the following functions. (See directions for more info.)
 
 def get_model_info(year):
     '''Takes in a year, and prints out each model, brand_name, and brand
     headquarters for that year using only ONE database query.'''
+    mdls = Model.query.options(db.joinedload('brands')).all()
 
-    pass
+    for model in mdls:
+        if model.year == year:
+        print model.name, model.brand.name, model.brand.headquarters
 
 def get_brands_summary():
     '''Prints out each brand name, and each model name for that brand
      using only ONE database query.'''
-
-    pass
+    mdls = Model.query.options(db.joinedload('brands')).all()
+    display = {}
+    for model in mdls:
+        display.get(str(model.brand_name), [""])
+        display[str(model.brand_name)].append(str(model.name)
+    print display
 
 # -------------------------------------------------------------------
 # Part 2.5: Discussion Questions (Include your answers as comments.)
